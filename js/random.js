3e86539c3764c52c7b5650131fa5ea3797b6627f
emilytrancat <123094758+emilytrancat@users.noreply.github.com>
2023-09-22 16:01:25 -0500

    Function Parameters and Arguments

--- js/random.js
+++ js/random.js
@@ -1,4 +1,4 @@
-// How to call a function:
+// HOW TO CALL A FUNCTION:
 
 // function alertRandomNumber() {
 //     const randomNumber = Math.floor( Math.random() * 6 ) +1;
@@ -9,11 +9,21 @@
 // alertRandomNumber();
 
 
-// How to return a function:
+// // HOW TO RETURN A FUNCTION:
 
-function getRandomNumber() {
-    const randomNumber = Math.floor( Math.random() * 6 ) +1;
+// function getRandomNumber() {
+//     const randomNumber = Math.floor( Math.random() * 6 ) +1;
+//     return randomNumber;
+// }
+// const dieRoll = getRandomNumber();
+// // console.log( getRandomNumber() );
+
+
+function getRandomNumber(upper) {
+    const randomNumber = Math.floor( Math.random() * upper ) +1;
     return randomNumber;
 }
-const dieRoll = getRandomNumber();
-// console.log( getRandomNumber() );
+console.log( getRandomNumber(6) );
+console.log( getRandomNumber(100) );
+console.log( getRandomNumber(1000) );
+console.log( getRandomNumber(21) );
