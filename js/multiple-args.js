101b71f9a3b3a875ccbbdb6718180d1c8ca4d590
emilytrancat <123094758+emilytrancat@users.noreply.github.com>
2023-09-25 12:38:10 -0500

    Pass Multiple Arguments to a Function
    and, "Create a max() Function"

---
+++ js/multiple-args.js
@@ -0,0 +1,15 @@
+function getArea(width, length, unit) {
+const area = width * length;
+return `${area} ${unit}`;
+}
+// // getArea(10, 20, 'sq ft');
+// // put this line of code above in the console
+
+/* Create a function named max that accepts two numbers as arguments (name the parameters whatever you would like.) The function
+return the larger of the two numbers */
+// function max( numberOne, numberTwo ) {
+//     if (numberOne === 5 ) {
+//         return numberOne;
+//     } else { return numberTwo; }
+// }
+// console.log ( max(6, 7) );
--- js/multiple-returns.js
+++ js/multiple-returns.js
@@ -1,3 +1,4 @@
+// ACTIVATE THIS: <input type="text" id="info" value="My info"> IN THE <MAIN> OF HTML TO SEE EXAMPLE:
 function isFieldEmpty() {
     const field = document.querySelector('#info');
     if (!field.value) {
@@ -13,8 +14,16 @@ if (fieldTest) {
     alert('Please provide your information.');
 }
 
+
+// ANOTHER EXAMPLE ON HOW RETURNS SHOULD BE LAST:
 // function noAlert() {
 //     return 5;
 //     alert("This won't appear");
+// }
+// noAlert();
+
+// function noAlert() {
+//     alert("This will appear");
+//     return 5;
 // }
 // noAlert();
